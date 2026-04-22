db60e27e9f933b87d2f8d33edbcfda678f821a93
gusderiv <gustavoadelgadillorivera@gmail.com>
2026-04-22 11:12:06 +0200

    he agregado color

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,10 @@ void main() {
 
     System.out.print("visual en github");
 
-}
+            String rojo = "\u001B[31m";
+            String reset = "\u001B[0m";
+
+            System.out.println(rojo + " Este texto es rojo" + reset);
+
+    }
+
